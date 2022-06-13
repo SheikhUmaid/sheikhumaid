50f9df3274cb6eb3b672846ba985d947db867025
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-06-13 22:19:56 +0530

    Update urls.py

--- porfolio/urls.py
+++ porfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('SheikhUmaid.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = 'SheikhUmaid.views.Page404Handler'
