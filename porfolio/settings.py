bdb7d1783f34dfb35a371b1bd527d6ec1d535ee7
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-06-13 22:31:36 +0530

    Update settings.py

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -122,14 +122,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
-
-
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR,'static')
-]
-STATIC_ROOT='staticfiles'
-MEDIA_DIR = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
